85d512a84a31b59bc3db748dd5f51979534c1849
maxlee <max.lee98@gmail.com>
2023-02-13 23:29:47 +0800

    Fixing Bank Account Display
    useEffect and API mismatch, corrected the mismatch to display the Account number, Account Type and Balance properly

--- express-bank-app/index.js
+++ express-bank-app/index.js
@@ -34,7 +34,6 @@ app.use(
 app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
